457b659e374bc00800756e1024e10d13c06a2b6e
Steve Lau <liuwenhong_steve@163.com>
2017-07-19 11:00:10 +0800

    Update Sales_item.h

--- exCode/include/Sales_item.h
+++ exCode/include/Sales_item.h
@@ -1,1 +1,37 @@
+/*
+ * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
+ * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
+ * 
+ * 
+ * 
+ * "The authors and publisher have taken care in the preparation of this book,
+ * but make no expressed or implied warranty of any kind and assume no
+ * responsibility for errors or omissions. No liability is assumed for
+ * incidental or consequential damages in connection with or arising out of the
+ * use of the information or programs contained herein."
+ * 
+ * Permission is granted for this code to be used for educational purposes in
+ * association with the book, given proper citation if and when posted or
+ * reproduced.Any commercial use of this code requires the explicit written
+ * permission of the publisher, Addison-Wesley Professional, a division of
+ * Pearson Education, Inc. Send your request for permission, stating clearly
+ * what code you would like to use, and in what specific way, to the following
+ * address: 
+ * 
+ *     Pearson Education, Inc.
+ *     Rights and Permissions Department
+ *     One Lake Street
+ *     Upper Saddle River, NJ  07458
+ *     Fax: (201) 236-3290
+*/ 
 
+/* This file defines the Sales_item class used in chapter 1.
+ * The code used in this file will be explained in
+ * Chapter 7 (Classes) and Chapter 14 (Overloaded Operators)
+ * Readers shouldn't try to understand the code in this file
+ * until they have read those chapters.
+*/
+
+#ifndef SALESITEM_H
+// we're here only if SALESITEM_H has not yet been defined
+#define 
