1d0f4c08a759f0908f578e392e6712c1328e78a7
Steve Lau <liuwenhong_steve@163.com>
2017-07-19 11:55:56 +0800

    Update Sales_item.h

--- exCode/include/Sales_item.h
+++ exCode/include/Sales_item.h
@@ -34,4 +34,97 @@
 
 #ifndef SALESITEM_H
 // we're here only if SALESITEM_H has not yet been defined
-#define 
+#define SALESITEM_H
+
+// Definition of Sales_item class and related functions goes here
+#include <iostream>
+#include <string>
+
+class Sales_item {
+// these declarations are explained section 7.2.1, p. 270 
+// and in chapter 14, pages 557, 558, 561
+friend std::istream& operator>>(std::istream&, Sales_item&);
+friend std::ostream& operator<<(std::ostream&, const Sales_item&);
+friend bool operator<(const Sales_item&, const Sales_item&);
+friend bool operator==(const Sales_item&, const Sales_item&);
+
+public:
+    // constructors are explained in section 7.1.4, pages 262 - 265
+    // default constructor needed to initialize members of built-in type
+    Sales_item() = default;
+    Sales_item(const std::string &book) : bookNo(book) {}
+    Sales_item(std::istream &is) { is >> *this; }
+
+public:
+    // operations on Sales_item objects
+    // member binary operator: left-hand operand bound to implicit this pointer
+    Sales_item& operator+=(const Sales_item&);
+    
+    // operations on Sales_item objects
+    std::string isbn() const { return bookNo; }
+    double avg_price() const;
+
+// private members as before
+private:
+    std::string bookNo;       // implicitly initialized to the empty string
+    unsigned units_sold = 0;  // explicitly initialized
+    double revenue = 0.0
+};
+
+// used in chapter 10
+inline bool compareIsbn(const Sales_item& lhs, const Sales_item& rhs)
+{
+    return lhs.isbn() == rhs.isbn();
+}
+
+// nonmember binary operator: must declare a parameter for each operand
+Sales_item operator+(const Sales_item&, const Sales_item&);
+
+inline bool operator==(const Sales_item& lhs, const Sales_item& rhs)
+{
+    // must be made a friend of Sales_item
+    return lhs.units_sold == rhs.units_sold && lhs.revenue == rhs.revenue && lhs.isbn() == rhs.isbn();           
+}
+
+inline bool operator!=(const Sales_item& lhs, const Sales_item& rhs)
+{
+    return !(lhs == rhs);    // != defined in terms of operator==
+}
+
+// assumes that both objects refer to the same ISBN
+Sales_item& Sales_item::operator+=(const Sales_item& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
